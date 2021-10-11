b115ecc1851c18df36a156b6c13e26178fa66132
Vince Blom <blomvin@gmail.com>
2021-10-11 17:16:51 -0500

    Fix some linting

--- src/web/client/src/router/index.js
+++ src/web/client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import NowPlaying from '../views/NowPlaying.vue'
 import Logs from '../views/Logs.vue'
 
